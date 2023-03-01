34a682c6e684b4f6e8782966d459bb35bfbb12dd
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-28 16:35:14 -0800

    added port

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const { root }  = require('./gql/resolvers.js')
 
 var app = express();
 var cors = require('cors');
-app.use( cors() );          // allow react app communicate with server on same machine/diff port
+app.use( cors() );   
+
+const port = process.env.PORT || 4000// allow react app communicate with server on same machine/diff port
 
 const v1 = {
   url: '/graphql',
@@ -19,6 +21,6 @@ const v1 = {
 const {url, config} = v1
 
 app.use(url, config);
-app.listen(4000, () => {
-  console.log('Running a GraphQL API server at localhost:4000/graphql');
+app.listen(port, () => {
+  console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
 });
